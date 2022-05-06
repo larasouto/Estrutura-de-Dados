67c95a746b50d22f5e5bb0a9b441dfbe6b8de569
Lara <81490646+larasouto@users.noreply.github.com>
2022-05-06 14:34:17 -0300

    Create Main.java

---
+++ Atividades/Atividade 01/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+
+        Ponto p1 = new Ponto(12,25);
+        Ponto p2 = new Ponto(24,-32);
+        System.out.println(p1.distEuclidiana(p2));
+    }
+}
+
