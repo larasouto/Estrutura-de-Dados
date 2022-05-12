cda7ef6dca7f06f792d8730497904364e5e5a35b
Lara <81490646+larasouto@users.noreply.github.com>
2022-05-11 21:00:48 -0300

    Create IAlunos.java

---
+++ Atividades/Atividade 04/IAlunos.java
@@ -0,0 +1,9 @@
+public interface IAlunos {
+    public void adiciona(Object elem);
+    public void imprime(Object elem);
+    public int tamanho();
+    public boolean contem(Object elem);
+    public boolean cheio();
+    public boolean remove(Object elem);
+    public void garanteEspaco();
+}
